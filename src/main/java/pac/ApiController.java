1b5fbffe2eedf58bec9c60153c64758583b88967
mikl745 <mikl745@yandex.ru>
2021-12-15 10:29:22 +0300

    Initial commit

---
+++ src/main/java/pac/ApiController.java
@@ -0,0 +1,10 @@
+package pac;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.*;
+
+@Controller
+@RestController
+public class ApiController {
+
+}
